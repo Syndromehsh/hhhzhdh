eabc03e17bcc9d37a82b3602eb2ff6ede63021ff
Syndromehsh <2872935391@qq.com>
2025-07-16 17:57:47 +0800

    Update List.lua

--- List.lua
+++ List.lua
@@ -2,5 +2,8 @@ local Server = {
     ["被遗弃"] = "https://raw.githubusercontent.com/Syndromehsh/BETA/refs/heads/main/NOL",
     ["力量传奇"] = "https://raw.github.com/114514541883484/GD/main/l",
     ["最强战场"] = "https://raw.githubusercontent.com/Syndromehsh/BETA/refs/heads/main/zuiqiang",
-} -- ["Server Name"] = "Server Url",
+} 
+
+
+return Server
 
